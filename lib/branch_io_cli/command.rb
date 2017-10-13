e9be6e72268affa65a26644370aec4d26ddbe977
Jimmy Dee <jgvdthree@gmail.com>
2017-10-13 16:59:59 -0700

    Infer location of project in validate command

--- lib/branch_io_cli/command.rb
+++ lib/branch_io_cli/command.rb
@@ -8,8 +8,14 @@ module BranchIOCLI
       end
 
       def validate(options)
+        path = xcodeproj_path options
+        unless path
+          say "Please specify the --xcodeproj option."
+          return
+        end
+
         # raises
-        xcodeproj = Xcodeproj::Project.open options.xcodeproj
+        xcodeproj = Xcodeproj::Project.open path
 
         valid = true
 
@@ -39,10 +45,45 @@ module BranchIOCLI
         valid &&= configuration_valid
 
         say "Universal Link configuration passed validation. ✅" if valid
+
+        # TODO: Return an exit code from the CLI indicating success or failure.
+        # e.g. if not valid
+        # $ branch_io validate
+        # $ echo $?
+        # 1
+        # if valid
+        # $ branch_io validate
+        # $ echo $?
+        # 0
       end
 
       def helper
         BranchIOCLI::Helper::BranchHelper
+      end
+
+      def xcodeproj_path(options)
+        return options.xcodeproj if options.xcodeproj
+
+        repo_path = "."
+
+        all_xcodeproj_paths = Dir[File.expand_path(File.join(repo_path, '**/*.xcodeproj'))]
+        # find an xcodeproj (ignoring the Pods and Carthage folders)
+        # TODO: Improve this filter
+        xcodeproj_paths = all_xcodeproj_paths.reject { |p| p =~ /Pods|Carthage/ }
+
+        # no projects found: error
+        say 'Could not find a .xcodeproj in the current repository\'s working directory.' and return nil if xcodeproj_paths.count == 0
+
+        # too many projects found: error
+        if xcodeproj_paths.count > 1
+          repo_pathname = Pathname.new repo_path
+          relative_projects = xcodeproj_paths.map { |e| Pathname.new(e).relative_path_from(repo_pathname).to_s }.join("\n")
+          say "Found multiple .xcodeproj projects in the current repository's working directory. Please specify your app's main project: \n#{relative_projects}"
+          return nil
+        end
+
+        # one project found: great
+        xcodeproj_paths.first
       end
     end
   end
