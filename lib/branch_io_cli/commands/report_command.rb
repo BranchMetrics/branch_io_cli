bcf2ca180d172f1f6dba4521b1e289a43f04b65e
Jimmy Dee <jgvdthree@gmail.com>
2017-11-01 10:45:21 -0700

    Report the version of CocoaPods if Podfile.lock found

--- lib/branch_io_cli/commands/report_command.rb
+++ lib/branch_io_cli/commands/report_command.rb
@@ -74,6 +74,7 @@ module BranchIOCLI
         return nil unless config_helper.podfile_path && File.exist?("#{config_helper.podfile_path}.lock")
         podfile_lock = Pod::Lockfile.from_file Pathname.new "#{config_helper.podfile_path}.lock"
         version = podfile_lock.version "Branch"
+
         version ? "#{version} [Podfile.lock]" : nil
       end
 
@@ -122,6 +123,11 @@ module BranchIOCLI
 
       def report_header
         header = `xcodebuild -version`
+
+        if config_helper.podfile_path && File.exist?("#{config_helper.podfile_path}.lock")
+          podfile_lock = Pod::Lockfile.from_file Pathname.new "#{config_helper.podfile_path}.lock"
+          header = "#{header}\nUsing CocoaPods v. #{podfile_lock.cocoapods_version}\n"
+        end
 
         podfile_requirement = requirement_from_podfile
         header = "#{header}\nFrom Podfile:\n#{podfile_requirement}\n" if podfile_requirement
