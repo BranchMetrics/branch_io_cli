a6235a15abc43f615f729d387637648269720e8c
Jimmy Dee <jgvdthree@gmail.com>
2017-11-02 11:29:44 -0700

    Refactored STDOUT.report_command. Recovered formatting at the terminal.

--- lib/branch_io_cli/core_ext/io.rb
+++ lib/branch_io_cli/core_ext/io.rb
@@ -1,13 +1,14 @@
 require "open3"
 
 class IO
+  # Report the command. Execute the command, capture stdout
+  # and stderr and report line by line. Report the exit
+  # status at the end in case of error. Returns a Process::Status
+  # object.
+  #
+  # :command: [String] a shell command to execute and report
   def report_command(command)
-    if self == STDOUT
-      # TODO: Improve this?
-      say "<%= color('$ #{command}', BOLD) %>\n\n"
-    else
-      write "$ #{command}\n\n"
-    end
+    write "$ #{command}\n\n"
 
     Open3.popen2e(command) do |stdin, output, thread|
       # output is stdout and stderr merged
@@ -21,8 +22,27 @@ class IO
       else
         write "#{status}\n\n"
       end
-
       return status
     end
+  end
+end
+
+# Report the command. Execute the command. Stdout and stderr are
+# not redirected. Report the exit status at the end if nonzero.
+# Returns a Process::Status object.
+#
+# :command: [String] a shell command to execute and report
+def STDOUT.report_command(command)
+  # TODO: Improve this?
+  say "<%= color('$ #{command}', BOLD) %>\n\n"
+  # May also write to stderr
+  # Could try system "#{command} 2>&1", but that might depend on the shell.
+  system command
+
+  status = $?
+  if status == 0
+    write "Success.\n\n"
+  else
+    write "#{status}\n\n"
   end
 end
