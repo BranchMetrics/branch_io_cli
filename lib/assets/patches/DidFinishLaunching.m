d099b6157fb8b3ce51883e8eca3b08e4b4213077
Jimmy Dee <jgvdthree@gmail.com>
2017-11-07 17:21:32 -0800

    Further ERB to reduce the number of patches.

--- lib/assets/patches/DidFinishLaunching.m
+++ lib/assets/patches/DidFinishLaunching.m
@@ -1,3 +1,8 @@
+<% if is_new_method %>
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+<% end %>
 <% if use_conditional_test_key? %>
 #ifdef DEBUG
     [Branch setUseTestBranchKey:YES];
@@ -7,3 +12,7 @@
         andRegisterDeepLinkHandlerUsingBranchUniversalObject:^(BranchUniversalObject *universalObject, BranchLinkProperties *linkProperties, NSError *error){
         // TODO: Route Branch links
     }];
+<% if is_new_method %>
+    return YES;
+}
+<% end %>
