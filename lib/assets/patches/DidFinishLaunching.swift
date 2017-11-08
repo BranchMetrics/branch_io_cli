df2f25301abfe4d3c0228c0c438e4cb37e8d684c
Jimmy Dee <jgvdthree@gmail.com>
2017-11-07 16:52:50 -0800

    Use the right template (and language)

--- lib/assets/patches/DidFinishLaunching.swift
+++ lib/assets/patches/DidFinishLaunching.swift
@@ -1,3 +1,9 @@
+<% unless config.keys.count <= 1 || has_multiple_info_plists? >
+        #if DEBUG
+            Branch.setUseTestBranchKey(true)
+        #endif
+
+<% end %>
         Branch.getInstance().initSession(launchOptions: launchOptions) {
             universalObject, linkProperties, error in
 
--- lib/assets/patches/DidFinishLaunchingNew.swift
+++ lib/assets/patches/DidFinishLaunchingNew.swift
@@ -1,10 +1,10 @@
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         <% unless config.keys.count <= 1 || has_multiple_info_plists? %>
-        #ifdef DEBUG
-            [Branch setUseTestBranchKey:YES];
-        #endif // DEBUG
-          
+        #if DEBUG
+            Branch.setUseTestBranchKey(true)
+        #endif
+
         <% end %>
         Branch.getInstance().initSession(launchOptions: launchOptions) {
             universalObject, linkProperties, error in
