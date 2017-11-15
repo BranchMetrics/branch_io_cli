e4c53de8306f7aa32f227888afae993fa1fefe11
Jimmy Dee <jgvdthree@gmail.com>
2017-11-15 14:48:43 -0800

    Better test for pod install

--- lib/branch_io_cli/command/setup_command.rb
+++ lib/branch_io_cli/command/setup_command.rb
@@ -30,9 +30,7 @@ module BranchIOCLI
           say "Universal Link configuration passed validation. ✅"
         end
 
-        begin
-          config.xcodeproj.build_configurations.first.debug?
-        rescue RuntimeError
+        if File.exist?(config.podfile_path) && config.pod_install_required?
           helper.verify_cocoapods
           say "Installing pods to resolve current build settings"
           Dir.chdir(File.dirname(config.podfile_path)) do
