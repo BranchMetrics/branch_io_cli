7456e5e90e9f30214c41e24477abd8b2e09af741
Jimmy Dee <jgvdthree@gmail.com>
2017-11-17 14:53:20 -0800

    More tests.

--- lib/branch_io_cli/configuration/option.rb
+++ lib/branch_io_cli/configuration/option.rb
@@ -64,7 +64,7 @@ module BranchIOCLI
         if type == Array
           value = value.split(",") if value.kind_of?(String)
         elsif type == String
-          value = value.strip
+          value = value.strip if value.kind_of?(String)
         elsif type.nil?
           value = true if value.kind_of?(String) && value =~ /^(true|yes)$/i
           value = false if value.kind_of?(String) && value =~ /^(false|no)$/i
--- spec/option_wrapper_spec.rb
+++ spec/option_wrapper_spec.rb
@@ -2,8 +2,17 @@ describe BranchIOCLI::Configuration::OptionWrapper do
   WRAPPER_CLASS = BranchIOCLI::Configuration::OptionWrapper
   let (:options) do
     [
-      BranchIOCLI::Configuration::Option.new(name: :foo, default_value: "bar")
+      BranchIOCLI::Configuration::Option.new(
+        name: :foo,
+        default_value: "bar",
+        env_name: "FOO",
+        type: String
+      )
     ]
+  end
+
+  before :each do
+    ENV["FOO"] = nil
   end
 
   it 'returns the value of any valid key in a hash supplied to the initializer as a method' do
@@ -17,7 +26,18 @@ describe BranchIOCLI::Configuration::OptionWrapper do
 
   it 'adds defaults if add_defaults is true' do
     wrapper = WRAPPER_CLASS.new({}, options)
-
     expect(wrapper.foo).to eq "bar"
+  end
+
+  it 'consults any env_value before default_value' do
+    ENV["FOO"] = "y"
+    wrapper = WRAPPER_CLASS.new({}, options)
+    expect(wrapper.foo).to eq "y"
+  end
+
+  it 'does not consult the environment or default_value if add_defaults is false' do
+    ENV["FOO"] = "y"
+    wrapper = WRAPPER_CLASS.new({}, options, false)
+    expect(wrapper.foo).to be_nil
   end
 end
