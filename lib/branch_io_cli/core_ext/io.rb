95f368ca5d7455e509bb1f09c9e6f28aafae24ac
Jimmy Dee <jgvdthree@gmail.com>
2017-11-30 09:48:05 -0800

    Just pass variadic args to system and popen2e

--- lib/branch_io_cli/core_ext/io.rb
+++ lib/branch_io_cli/core_ext/io.rb
@@ -7,7 +7,7 @@ class IO
   # status at the end in case of error. Returns a Process::Status
   # object.
   #
-  # @param command [String, Array] a shell command to execute and report
+  # @param command a shell command to execute and report
   def log_command(*args)
     if args.count == 1
       command = args.first
@@ -18,7 +18,7 @@ class IO
 
     write "$ #{command}\n\n"
 
-    Open3.popen2e(command) do |stdin, output, thread|
+    Open3.popen2e(*args) do |stdin, output, thread|
       # output is stdout and stderr merged
       while (line = output.gets)
         puts line
@@ -39,7 +39,7 @@ end
 # not redirected. Report the exit status at the end if nonzero.
 # Returns a Process::Status object.
 #
-# @param command [String, Array] a shell command to execute and report
+# @param command a shell command to execute and report
 def STDOUT.log_command(*args)
   if args.count == 1
     command = args.first
@@ -51,8 +51,7 @@ def STDOUT.log_command(*args)
   # TODO: Improve this implementation?
   say "<%= color(%q{$ #{command}}, [MAGENTA, BOLD]) %>\n\n"
   # May also write to stderr
-  # Could try system "#{command} 2>&1", but that might depend on the shell.
-  system command
+  system(*args)
 
   status = $?
   if status == 0
