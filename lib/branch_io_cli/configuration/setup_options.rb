29cc937da709058934904e13f9dceed62e6cf996
Jimmy Dee <jgvdthree@gmail.com>
2017-12-12 15:41:39 -0800

    Change the example carthage command.

--- lib/branch_io_cli/configuration/setup_options.rb
+++ lib/branch_io_cli/configuration/setup_options.rb
@@ -95,7 +95,7 @@ module BranchIOCLI
             Option.new(
               name: :carthage_command,
               description: "Command to run when installing from Carthage",
-              example: "update --platform ios",
+              example: "update --no-use-binaries",
               type: String,
               default_value: "bootstrap --platform ios"
             ),
