5aec676c0574111b95bb69937ce743d662f5cb15
Jimmy Dee <jgvdthree@gmail.com>
2017-12-12 15:09:07 -0800

    Improvement for Carthage: Only supply ios-branch-deep-linking when using update or build command

--- lib/branch_io_cli/helper/tool_helper.rb
+++ lib/branch_io_cli/helper/tool_helper.rb
@@ -220,7 +220,9 @@ github "BranchMetrics/ios-branch-deep-linking"
           return false unless PatchHelper.patch_cartfile cartfile_path
 
           # 2. carthage update
-          sh "carthage #{options.carthage_command} ios-branch-deep-linking", chdir: File.dirname(config.cartfile_path)
+          cmd = "carthage #{options.carthage_command}"
+          cmd << " ios-branch-deep-linking" if options.carthage_command =~ /^(update|build)/
+          sh cmd, chdir: File.dirname(config.cartfile_path)
 
           # 3. Add Cartfile and Cartfile.resolved to commit (in case :commit param specified)
           helper.add_change cartfile_path
